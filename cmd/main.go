6058a70fec15973f3465e35c8d76ca5e15bed286
Oleg Volkov <voa91ua@gmail.com>
2023-02-01 21:07:56 +0200

    update main file

--- cmd/main.go
+++ cmd/main.go
@@ -14,39 +14,31 @@ const (
 func main() {
 	cfg, err := config.Init()
 
-	fmt.Println("CONFIG", cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	newCl := apiclient.Start(cfg)
 
-	//users, err := newCl.GetUsers()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//women := users.FilterByGender(female)
-	//if women == nil {
-	//	log.Fatal("no users found")
-	//}
-	//
-	//for _, woman := range women {
-	//	fmt.Printf("Name: %s\nEmail: %s\nGender: %s\n\n", woman.Name, woman.Email, woman.Gender)
-	//}
+	users, err := newCl.GetUsers()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//newCl.CreateUser(apiclient.User{
-	//	Name:   "oleg",
-	//	Email:  "oleg@i.ua",
-	//	Gender: "male",
-	//	Status: "active",
-	//})
+	women := users.FilterByGender(female)
+	if women == nil {
+		log.Fatal("no users found")
+	}
+
+	for _, woman := range women {
+		fmt.Printf("Name: %s\nEmail: %s\nGender: %s\n\n", woman.Name, woman.Email, woman.Gender)
+	}
 
 	if err := newCl.CreateUser(apiclient.User{
-		Name:   "olejka",
-		Email:  "olejka@i.ua",
-		Gender: "female",
-		Status: "inactive",
+		Name:   "oleg",
+		Email:  "oleg@i.ua",
+		Gender: "male",
+		Status: "active",
 	}); err != nil {
 		log.Fatal(err)
 	}
